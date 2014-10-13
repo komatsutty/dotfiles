97c0a9b7af9bcda83bb31f402839c7d7d6a7eaf2
komatsutty <git@komatsutty.com>
2014-10-13 03:12:00 +0000

    Add .screenrc

--- dotfiles_symlink.sh
+++ dotfiles_symlink.sh
@@ -4,4 +4,5 @@ ln -sF ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sF ~/dotfiles/.vimrc ~/.vimrc
 ln -sF ~/dotfiles/.vim ~/.vim
 ln -sF ~/dotfiles/.inputrc ~/.inputrc
+ln -sF ~/dotfiles/.screenrc ~/.screenrc
 
